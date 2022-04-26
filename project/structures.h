e10601becd21462d592d17d4fd9140234986afbf
Wayne Yuan <44449663+wayney15@users.noreply.github.com>
2022-04-26 10:23:29 -0500

    added structures and make file

---
+++ project/structures.h
@@ -0,0 +1,65 @@
+#ifndef ITEM_STRUCTURES
+#define ITEM_STRUCTURES
+
+struct Laptops
+{ 
+    char brandName[25];
+    char processorBrand[25];
+    char processorModel[25];
+    char graphicCardBrand[25];
+    char graphicCardModel[25];
+    char operatingSystem[25];
+    char RAM[25];
+    char storage[25];
+    float price;
+};
+
+struct Cameras
+{ 
+    char brandName[25];
+    char ModelNumber[25];
+    char cameraType[25];
+    float width;
+    float height;
+    float depth;
+    char color[25];
+    char sensorType[25];  //COMS, Live MOS
+    char sensorSize[25]; // Four Thirds System, APS-C
+    char effectivePixels[25]; // 16.1 megapixels
+    char storage[25];  // 125 GB
+    float price;
+};
+
+struct Phones
+{ 
+    char brandName[25];
+    char ModelNumber[25];
+    char storage[25];  // 125 GB
+    float width;
+    float height;
+    char color[25];
+    float price;
+};
+
+struct Watches
+{ 
+    char brandName[25];
+    char ModelNumber[25];
+    char type[25];  // mechanical, quartz, automatical
+    float weight;
+    char color[25];
+    float price;
+};
+
+struct refrigerators
+{ 
+    char brandName[25];
+    char ModelNumber[25];
+    char doorStyle[25];  // frech door, side-by-side
+    float width;
+    float height;
+    float depth;
+    char color[25];
+    float price;
+};
+#endif
