63ca6d030ba7090b812716eac85b7203f3dcc0a2
gopi krishnan manikka subramanian <gauthamkv@rodeodigital.com>
2022-05-02 23:03:47 -0400

    search modified

--- project/search_results.c
+++ project/search_results.c
@@ -6,5 +6,13 @@
 #include "admin_menu.h"
 
 void searchResults(MYSQL *conn , char product[25], char brand[25]) {
-    printf("product : %s , brand: %s");
+    printf("product : %s , brand: %s \n");
+    printf("Search results: \n");
+
+    char query[500],queryPt1[500],queryPt2[500];
+    strcpy(queryPt1, "SELECT distinct id, ");
+    strcpy(queryPt1, strcat(queryPt1, product));
+    strcpy(queryPt2,", price  FROM ");
+    strcpy(query, strcat(queryPt1,queryPt2));
+    printf("%s\n\n", query);
 }
