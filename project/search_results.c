31a82ee15fbf360bff8a0e8af4f5b9004c1badb1
gopi krishnan manikka subramanian <gauthamkv@rodeodigital.com>
2022-05-03 00:27:00 -0400

    search modified

--- project/search_results.c
+++ project/search_results.c
@@ -13,12 +13,13 @@ struct cart * searchResults(MYSQL *conn , char product[25], char brand[25], stru
     printf("Search results: \n");
 
     char query[500],queryPt1[500],queryPt2[500];
-    strcpy(queryPt1, "SELECT distinct id, brandName, price FROM ");
-    strcpy(queryPt1, strcat(queryPt1, product));
-    strcpy(queryPt2, " WHERE brandName = \"");
-    strcpy(queryPt2, strcat(queryPt2, brand));
-    strcpy(queryPt2, strcat(queryPt2, "\";"));
-    strcpy(query, strcat(queryPt1,queryPt2));
+    // strcpy(queryPt1, "SELECT distinct id, brandName, price FROM ");
+    // strcpy(queryPt1, strcat(queryPt1, product));
+    // strcpy(queryPt2, " WHERE brandName = \"");
+    // strcpy(queryPt2, strcat(queryPt2, brand));
+    // strcpy(queryPt2, strcat(queryPt2, "\";"));
+    // strcpy(query, strcat(queryPt1,queryPt2));
+    sprintf(query, "SELECT distinct id, brandName, price FROM %s WHERE brandName = \"%s\"", product, brand);
 
     if (mysql_query(conn, query)) {
         connection_error(conn);
