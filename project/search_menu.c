5fe6dab154f1ab8ff31b4ae05f4d385e1de5283d
gopi krishnan manikka subramanian <gauthamkv@rodeodigital.com>
2022-05-03 01:19:21 -0400

    search modified

--- project/search_menu.c
+++ project/search_menu.c
@@ -107,3 +107,14 @@ struct cart * searchMenu(MYSQL *conn, struct cart *head) {
     }
     return head;
 }
+
+struct cart * head searchLaptopBrands(MYSQL *conn , struct cart * head, char table[25], char brand[25]) {
+    char query[500];
+    sprintf(query, "select distinct processorBrand from %s where brandName = \"%s\"", table, brand);
+    MYSQL_RES *result = mysql_store_result(conn);
+    if (!result) {
+        printf("Couldn't get results set: %s\n", mysql_error(conn));
+        return head;
+    }
+    return head;
+}
