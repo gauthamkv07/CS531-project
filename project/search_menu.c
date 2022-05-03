7e23fdae4febac0d2ed500fff690a4c19fdfd3db
gopi krishnan manikka subramanian <gauthamkv@rodeodigital.com>
2022-05-03 02:02:42 -0400

    search modified

--- project/search_menu.c
+++ project/search_menu.c
@@ -53,60 +53,6 @@ struct cart * searchMenu(MYSQL *conn, struct cart *head) {
     }
     
     if(choice <index){
-        if(!strcmp(tables[choice-1],"laptops")) {
-            char selectQuery[50];
-            char table[25];
-            strcpy(table, tables[choice - 1]);
-            strcpy(selectQuery, "SELECT distinct brandName FROM ");
-            strcpy(query, strcat(selectQuery , tables[choice-1]));
-            if (mysql_query(conn, query)) {
-                connection_error(conn);
-                return head;
-            } else {
-                MYSQL_RES *result = mysql_store_result(conn);
-                if (!result) {
-                    printf("Couldn't get results set: %s\n", mysql_error(conn));
-                } else {
-                    MYSQL_ROW row = mysql_fetch_row(result);
-                    if(!row){
-                        printf("This product is not available right now. \n\n");
-                        return head;
-                    }
-                    char brands[12][25];
-                    index = 1;
-                    printf("\nBrands: \n");
-                    printf("%d.", index);
-                    strncpy(brands[index-1],row[0],25);
-                    puts(row[0]);
-                    index++;
-                    while (row = mysql_fetch_row(result)) {
-                        printf("%d.", index);
-                        strncpy(brands[index-1],row[0],25);
-                        puts(row[0]);
-                        index++;
-                    }
-                    printf("%d.Back to the main menu\n\n", index);
-                    mysql_free_result(result);
-
-                    choice = 0;
-                    while(choice > index+1 || choice == 0) {
-                        printf("Choose brand: ");
-                        fgets(buff, 100, stdin);
-                        choice = atoi(buff);
-                        if(choice > index + 1 || choice == 0) {
-                            printf("Choose an valid option.\n");
-                        }
-                    }
-                    char brand[25];
-                    strcpy(brand, brands[choice - 1]);
-
-                    if(choice < index) {
-                            // return searchLaptopBrands(conn, head, table,brand);
-                    }
-                }
-            } 
-        }
-        if(!strcmp(tables[choice-1],"laptops")) {
            char selectQuery[50];
             char table[25];
             strcpy(table, tables[choice - 1]);
@@ -154,11 +100,12 @@ struct cart * searchMenu(MYSQL *conn, struct cart *head) {
                     strcpy(brand, brands[choice - 1]);
 
                     if(choice < index) {
+                        if(!strcmp(tables[choice-1],"laptops")) {
                             return searchLaptopBrands(conn, head, table,brand);
+                        }
                     }
                 }
-            } 
-        }
+            }
     }
     return head;
 }
