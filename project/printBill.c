9c54c761511fa28318b6a0890f9a88331336cafc
gopi krishnan manikka subramanian <gauthamkv@rodeodigital.com>
2022-05-03 03:43:08 -0400

    search modified

--- project/printBill.c
+++ project/printBill.c
@@ -10,14 +10,14 @@ void printBill(struct cart *head,double total,double tax) {
     struct cart *temp = head;
 
         fprintf(fp,"                 BILL                     \n");
-        fprintf(fp,"id    name                     qty   price\n");
+        printf("id    name                                              qty   price\n");
 
         while(temp!=NULL) {
-            fprintf(fp,"%d    %-25s  %d    %-10.2f\n", temp->itemId, temp->name,  temp->qty, temp->price);
+            printf("%d    %-50s  %d    %-10.2f\n", temp->itemId, temp->prodName,  temp->qty, temp->price);
             temp = temp -> next;
         }
-        fprintf(fp,"tax :                                %.2f\n", tax);
-        fprintf(fp,"total :                              %.2f\n", total);
+        printf("tax :                                                         %.2f\n", tax);
+        printf("total :                                                       %.2f\n", total);
 
         fprintf(fp,"\n        *********THANK YOU*********");
 
