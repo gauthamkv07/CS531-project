a751915b8f4c5a2534ae1e4d8cac6e0c2524ae7a
gopi krishnan manikka subramanian <gauthamkv@rodeodigital.com>
2022-05-03 15:27:21 -0400

    search modified

--- project/search_watches_results.c
+++ project/search_watches_results.c
@@ -62,7 +62,7 @@ struct cart * searchWatchesResults(MYSQL *conn , char product[25], char brand[25
                 strncpy(color[index-1], row[4], 25);
                 strncpy(weight[index-1], row[5], 25);
                 strncpy(price[index-1], row[6], 25);
-                printf(" %-5d %-10s %-11s %-8s %-11s %-16s \n", index,brandName[index-1], ModelNumber[index-1],type[index-1],color[index-1],weight[index-1],price[index -1]);
+                printf(" %-5d %-10s %-22s %-15s %-6s %-10s %-16s \n", index,brandName[index-1], ModelNumber[index-1],type[index-1],color[index-1],weight[index-1],price[index -1]);
                 index++;
             }
             printf(" %d. Back to main menu\n\n", index);
